f43f8c02e2e82c9a39260356cb24a32db4e5d67e
Hessel <h.a.middendorp@icloud.com>
2025-08-22 14:20:18 +0200

    archief

---
+++ src/routes/archief/+page.server.ts
@@ -0,0 +1,90 @@
+import { client } from '$lib/sanity';
+import type { PageServerLoad } from './$types';
+import { error } from '@sveltejs/kit';
+
+export interface PostListItem {
+	_id: string;
+	title: string;
+	date: string;
+	slug: {
+		current: string;
+	};
+	categories: { name: string; slug: { current: string } }[];
+	featuredMedia?: {
+		asset: {
+			url: string;
+			metadata: {
+				lqip: string;
+			};
+		};
+		alt?: string;
+	};
+	estimatedReadingTime: number;
+}
+
+type QueryResult = {
+	name: string;
+	posts: PostListItem[];
+	totalPosts: number;
+} | null;
+
+// Define how many posts to show per page
+const pageSize = 10;
+
+export const load: PageServerLoad = async ({ url }) => {
+	// Get the current page number from the URL, default to 1
+	const page = Number(url.searchParams.get('page')) || 1;
+
+	// Calculate the start and end indices for the GROQ query
+	const start = (page - 1) * pageSize;
+	const end = page * pageSize;
+
+	const groqQuery = `
+	{
+  // Page of posts
+  "posts": *[_type == "post"] | order(date desc)[$start...$end]{
+  _id,
+  title,
+  date,
+  slug,
+  categories[]->{
+    name,
+    slug
+  },
+  "featuredMedia": {
+    "asset": featuredMedia.asset->{
+      url,
+      metadata { lqip }
+    },
+    "alt": featuredMedia.alt
+  },
+  "estimatedReadingTime": round(length(pt::text(content)) / 5 / 180 )
+  },
+
+  "totalPosts": count(*[_type == "post"])
+}
+
+  `;
+
+	const data = await client.fetch<QueryResult>(groqQuery, {
+		start: start,
+		end: end
+	});
+
+	if (!data) {
+		throw error(404, `Page  not found`);
+	}
+
+	const totalPages = Math.ceil(data.totalPosts / pageSize);
+
+	// If a user tries to access a page that doesn't exist, show a 404
+	if (page > totalPages && data.totalPosts > 0) {
+		throw error(404, 'Page not found');
+	}
+
+	return {
+		posts: data.posts,
+		currentPage: page,
+		totalPages: totalPages
+	};
+};
