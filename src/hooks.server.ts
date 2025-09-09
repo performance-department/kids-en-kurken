dfd09dbab7ee9ad4fcf4dcaaf9449fb5bd51b82b
Hessel <h.a.middendorp@icloud.com>
2025-09-09 10:10:08 +0200

    comment out hooks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,4 +1,4 @@
-import { handlePreview } from '@sanity/visual-editing/svelte';
-import { serverClient } from '$lib/server/sanity';
+// import { handlePreview } from '@sanity/visual-editing/svelte';
+// import { serverClient } from '$lib/server/sanity';
 
-export const handle = handlePreview({ client: serverClient });
+// export const handle = handlePreview({ client: serverClient });
