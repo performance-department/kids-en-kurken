9ebb9f6e9d07e500721025d29b9a5e0c4d315148
Hessel <h.a.middendorp@icloud.com>
2025-09-23 15:25:20 +0200

    clenaup

--- src/hooks.server.ts
+++
@@ -1,4 +0,0 @@
-// import { handlePreview } from '@sanity/visual-editing/svelte';
-// import { serverClient } from '$lib/server/sanity';
-
-// export const handle = handlePreview({ client: serverClient });
